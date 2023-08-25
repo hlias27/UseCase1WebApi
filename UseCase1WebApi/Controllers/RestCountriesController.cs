7c302d592db436558e4a16e95dd534e9080d9e01
Halyna Leshchynska <hlias@softserveinc.com>
2023-08-25 12:20:33 +0300

    UC#1 Create endpoint

--- UseCase1WebApi/Controllers/RestCountriesController.cs
+++ UseCase1WebApi/Controllers/RestCountriesController.cs
@@ -18,7 +18,6 @@ public class RestCountriesController : ControllerBase
 	public async Task<IEnumerable<object>?> Get(string? param1 = null, int? param2 = null, string? param3 = null)
 	{
 		using var httpClient = new HttpClient();
-
 		var response = await httpClient.GetAsync(URL);
 		var countriesString = await response.Content.ReadAsStringAsync();
 
